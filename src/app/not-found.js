448ceb8abd7d805ff8ce89edbdf4d1e30af0ca0d
OXAYR <uzairmaqsood451@gmail.com>
2025-05-19 11:16:27 +0500

    not found added

---
+++ src/app/not-found.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center text-primary h-screen">
+        <h1 className="text-4xl font-bold mb-4">404</h1>
+        <p className="text-xl">Oops! Page not found.</p>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
